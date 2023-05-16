e9272d3f3d7973a599e0149c3921144c8357b342
Ola Abouelhadid <abouelhadid.ola@gmail.com>
2023-05-16 23:40:46 +0300

    phrase searching
    phrase searching inside the crawler
    
    Co-Authored-By: Donia Remah <75097598+DoniaRemah@users.noreply.github.com>
    Co-Authored-By: Rana Moataz <108206592+ranamoataz3@users.noreply.github.com>

--- src/Server.js
+++ src/Server.js
@@ -84,12 +84,23 @@ app.get('/search',async (req,res)=>{
 
     //TODO: Implement phrase search
     //will search for the exact phrase in the document of crawler
+    var isPhraseSearch=false;
     
     //start time
     const start=Date.now();
 
     //the query is the whole string containing the words that the user is searching for
-    const query=req.query.query;
+    var query=req.query.query;
+
+    //check if the first character and the last character is a quotation mark
+    if(query[0]==='"' && query[query.length-1]==='"')
+    {
+        //phrase search
+        isPhraseSearch=true;    
+        //remove the quotation marks from the query
+        query=query.slice(1,query.length-1);
+    }
+
     //pagination query parameters
     const page=parseInt(req.query.page);
     const limit=parseInt(req.query.limit) || 10; 
@@ -118,110 +129,218 @@ app.get('/search',async (req,res)=>{
     }
 
     ///////////////////////////////////////////////////////////////Preprocessing on query///////////////////////////////////////////////
-    //1- Remove all special characters and numbers using lodash
-    const query1=lodash.replace(query, /[^a-zA-Z ]/g, "");
 
-    //2- Remove all the stop words
-    //the stop words are stored in a file called StopWords.txt
-    //read the file, one word per line
-    const stopWords=fs.readFileSync('StopWords.txt','utf8').split('\r\n');
-    // console.log(stopWords);
-    //remove the stop words from the query
-    const query2=lodash.difference(query1.split(' '),stopWords).join(' ');
+    //case of phrase search
+    if(isPhraseSearch){ 
+        //remove stop words
+        const stopWords=fs.readFileSync('StopWords.txt','utf8').split('\r\n');
+        const query1=lodash.difference(query.split(' '),stopWords).join(' ');
+        //lowercase
+        const query2=query1.toLowerCase();
 
+        //for Crawler collection in the database
+        //search for the query in the value inside the Content field
 
-    //3- lowercase all the words
-    const query3=query2.toLowerCase();
-
-    //4- remove the duplicate words
-    const query4=lodash.uniq(query3.split(' ')).join(' ');
-
-
-    //5- split the query into words
-    const queryWords=query4.split(' ');
-
-    //6- Stemming
-    // const stemmer = new EnglishStemmer();
-    const stemmer = Snowball.newStemmer('english');
-    const finalQueryWords =queryWords.map(word=>stemmer.stem(word));
-
-    console.log(finalQueryWords);
-
-    ///////////////////////////////////////////////////////////////Search the database///////////////////////////////////////////////
-    const foundDocuments=[];
-    //for every word in the query, search the database for the documents that contain this word
-    //inside the the Indexer collection
-    //search for the value in the Word field
-    // if the word is found, push the FoundInDocs field array elements to the foundDocuments array
-    // access the database
-    for(let i=0;i<finalQueryWords.length;i++){
-        const foundWord=await database.collection("Indexer").find({Word:finalQueryWords[i]}).toArray();
-        // console.log(foundWord);
-
-        
-        if(foundWord.length>0){
-            foundWord[0].FoundInDocs.forEach(object => {
-                foundDocuments.push(object);
-            });    
+        const foundDocuments=await database.collection("Crawler").find({Content:{$regex:`${query2}`}}).toArray();
+        if (foundDocuments.length==0){
+            console.log('No results found');
+            return res.status(200).json({message:'No results found for this query'});
         }
+        else{
+            //initialize the response object
+            const response={
+                result:[],
+                pagination:{},
+                time:0
+            };
+            //pagination
+            const startIndex=(page-1)*limit;
+            const endIndex=page*limit;
+
+            //slice the array
+            const results=foundDocuments.slice(startIndex,endIndex);
+
+            const totalResults=foundDocuments.length;
+            const totalPages=Math.ceil(totalResults/limit);
+    
+            response.pagination.totalResults=totalResults;
+            response.pagination.totalPages=totalPages;
+            response.pagination.currentPage=page;
+            response.pagination.nextPage=page < totalPages ? page + 1 : null,
+            response.pagination.previousPage=page > 1 ? page - 1 : null
+
+            //the foundDocuments format is an array of objects
+            results.forEach(object => {
+                var tempObject={};
+                tempObject.URL=object.URL;
+                tempObject.Title=object.Title;
+                //find the index of the query in the content
+                const index=object.Content.indexOf(query2);
+                //take a snippet around the query
+                //the snippet is 200 characters long
+                //the snippet is centered around the query
+                //check the cases of index
+                if (index-400<0){
+                    if(index+400<object.Content.length){
+                        tempObject.Content=object.Content.slice(0,index+400);
+                    }
+                    else{
+                        tempObject.Content=object.Content.slice(0,index);
+                    }
+                }
+                else if(index+400>object.Content.length){
+                    if(index-400>0){
+                        tempObject.Content=object.Content.slice(index-400,index);
+                    }
+                }
+                else{
+                    tempObject.Content=object.Content.slice(index-400,index+400);
+                }
+
+                //push the tempObject to the response result array
+                response.result.push(tempObject);
+            });
+
+            //send the response
+            try{
+                //end time
+                const end=Date.now();
+                response.time=parseFloat(((end-start)*0.001).toFixed(3));
+                return res.status(200).json(response);
+            }
+            catch(error){
+                console.log('Error in returning the response of this phrase search');
+            }
+            
+        }
+
     }
-    // console.log(foundDocuments);
+    else
+    {
+        //case of not phrase search 
+        //1- Remove all special characters and numbers using lodash
+        const query1=lodash.replace(query, /[^a-zA-Z ]/g, "");
 
-    ///////////////////////////////////////////////////////////////Ranking the documents///////////////////////////////////////////////
-
-    //rank based on the IDF_TF value inside the array of objects
-    //the array of objects is the FoundInDocs field
-    //the IDF_TF value is the IDF_TF field
-    //sort the array
-    //the first element is the document with the highest rank
-    //the last element is the document with the lowest rank
-    //the array is sorted in descending order
-
-    // TODO: Sort according to the IDF_TF value
-    // TODO: for snippet, search for the actual query in the original document from crawler for accuracy
+        //2- Remove all the stop words
+        //the stop words are stored in a file called StopWords.txt
+        //read the file, one word per line
+        const stopWords=fs.readFileSync('StopWords.txt','utf8').split('\r\n');
+        // console.log(stopWords);
+        //remove the stop words from the query
+        const query2=lodash.difference(query1.split(' '),stopWords).join(' ');
 
 
-    ///////////////////////////////////////////////////////////////Return the results///////////////////////////////////////////////
-    //initializing the response object
-    const response={
-        result:[],
-        pagination:{},
-        time:0
-    };
-    //pagination
-    const startIndex=(page-1)*limit;
-    const endIndex=page*limit;
-    //slice the array
-    const results=foundDocuments.slice(startIndex,endIndex);
-    //return the results
-    response.result=results;
+        //3- lowercase all the words
+        const query3=query2.toLowerCase();
 
-    const totalResults=foundDocuments.length;
-    const totalPages=Math.ceil(totalResults/limit);
+        //4- remove the duplicate words
+        const query4=lodash.uniq(query3.split(' ')).join(' ');
 
-    response.pagination.totalResults=totalResults;
-    response.pagination.totalPages=totalPages;
-    response.pagination.currentPage=page;
-    response.pagination.nextPage=page < totalPages ? page + 1 : null,
-    response.pagination.previousPage=page > 1 ? page - 1 : null
 
-    //remove the IDF_TF field from all the objects of the response result array
-    response.result.forEach(object => {
-        delete object.IDF_TF;
-    });
+        //5- split the query into words
+        const queryWords=query4.split(' ');
 
-    //return the response
-    try{
-        //end time
-        const end=Date.now();
-        response.time=parseFloat(((end-start)*0.001).toFixed(3));
-        return res.status(200).json(response);
-    }
-    catch(error){
-        console.log('Error in returning the response');
+        //6- Stemming
+        // const stemmer = new EnglishStemmer();
+        const stemmer = Snowball.newStemmer('english');
+        const finalQueryWords =queryWords.map(word=>stemmer.stem(word));
+
+        console.log(finalQueryWords);
+
+        ///////////////////////////////////////////////////////////////Search the database///////////////////////////////////////////////
+        const foundDocuments=[];
+        //for every word in the query, search the database for the documents that contain this word
+        //inside the the Indexer collection
+        //search for the value in the Word field
+        // if the word is found, push the FoundInDocs field array elements to the foundDocuments array
+        // access the database
+        for(let i=0;i<finalQueryWords.length;i++){
+            const foundWord=await database.collection("Indexer").find({Word:finalQueryWords[i]}).toArray();
+            // console.log(foundWord);
+
+            
+            if(foundWord.length>0){
+                foundWord[0].FoundInDocs.forEach(object => {
+                    foundDocuments.push(object);
+                });    
+            }
+        }
+        // console.log(foundDocuments);
+
+        ///////////////////////////////////////////////////////////////Ranking the documents///////////////////////////////////////////////
+
+        //rank based on the IDF_TF value inside the array of objects
+        //the array of objects is the FoundInDocs field
+        //the IDF_TF value is the IDF_TF field
+        //sort the array
+        //the first element is the document with the highest rank
+        //the last element is the document with the lowest rank
+        //the array is sorted in descending order
+
+        // TODO: Sort according to the IDF_TF value
+        // TODO: for snippet, search for the actual query in the original document from crawler for accuracy
+
+
+        ///////////////////////////////////////////////////////////////Return the results///////////////////////////////////////////////
+        //initializing the response object
+        const response={
+            result:[],
+            pagination:{},
+            time:0
+        };
+        //pagination
+        const startIndex=(page-1)*limit;
+        const endIndex=page*limit;
+        //slice the array
+        const results=foundDocuments.slice(startIndex,endIndex);
+        //return the results
+        response.result=results;
+
+        const totalResults=foundDocuments.length;
+        const totalPages=Math.ceil(totalResults/limit);
+
+        response.pagination.totalResults=totalResults;
+        response.pagination.totalPages=totalPages;
+        response.pagination.currentPage=page;
+        response.pagination.nextPage=page < totalPages ? page + 1 : null,
+        response.pagination.previousPage=page > 1 ? page - 1 : null
+
+        //remove the IDF_TF field from all the objects of the response result array
+        response.result.forEach(object => {
+            delete object.IDF_TF;
+        });
+
+        //return the response
+        try{
+            //end time
+            const end=Date.now();
+            response.time=parseFloat(((end-start)*0.001).toFixed(3));
+            return res.status(200).json(response);
+        }
+        catch(error){
+            console.log('Error in returning the response');
+        }
     }
 
 });
+
+//function to insert in crawler collection
+//FOR TESTING ONLY
+app.get('/insertInCrawler',async (req,res)=>{
+    //get the request.body
+    const body=req.body;
+    //insert the body in the database
+    try{
+        await database.collection("Crawler").insertOne(body);
+        console.log('The document is inserted in the crawler collection');
+        res.status(200).json({message:'The document is inserted in the crawler collection'});
+    }
+    catch(error){
+        console.log('Error in inserting the document in the crawler collection');
+        res.status(500).json({message:'Error in inserting the document in the crawler collection'});
+    }
+});
+
 
 
 
